24f30b1d7007e45f7bf120e010d1b2eeff1666d8
Olena Fito <olenkafito@gmail.com>
2019-11-15 09:14:15 +0200

    add lab 5 code - class that implements buble sorting in columns as c

---
+++ main.cpp
@@ -0,0 +1,135 @@
+//
+//  main.cpp
+//  Lab5
+//
+//  Created by Олена Фітьо on 11/4/19.
+//  Copyright © 2019 Олена Фітьо. All rights reserved.
+//
+
+#include <iostream>
+#include <cstdlib>
+#define n 5
+
+using namespace std;
+
+class Array
+{
+private:
+    double dArr[n];
+public:
+    friend void InputArray(Array arr[]);
+    friend void OutputArray(Array arr[]);
+    friend void SortArray(Array arr[]);
+    friend void CalcArray(Array arr[]);
+};
+void InputArray(Array  arr[])
+{
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+        {
+            cout << "[" << i + 1 << "][" << j + 1  << "] = ";
+            cin >> arr[i].dArr[j];
+        }
+}
+void OutputArray(Array  arr[])
+{
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            cout << arr[j].dArr[i] << "\t";
+        }
+        cout << endl;
+    }
+}
+
+
+void SortArray(Array  arr[])
+{
+    for (int i = 0; i < n; ++i)
+    {
+        for (int BlockSizeIterator = 1; BlockSizeIterator < n; BlockSizeIterator *= 2)
+        {
+            for (int BlockIterator = 0; BlockIterator < n - BlockSizeIterator; BlockIterator += 2 * BlockSizeIterator)
+            {
+                int LeftBlockIterator = 0;
+                int RightBlockIterator = 0;
+                int LeftBorder = BlockIterator;
+                int MidBorder = BlockIterator + BlockSizeIterator;
+                int RightBorder = BlockIterator + 2 * BlockSizeIterator;
+                RightBorder = (RightBorder < n) ? RightBorder : n;
+                int* SortedBlock = new int[RightBorder - LeftBorder];
+                
+                while (LeftBorder + LeftBlockIterator < MidBorder && MidBorder + RightBlockIterator < RightBorder)
+                {
+                    if (arr[LeftBorder + LeftBlockIterator].dArr[i] > arr[MidBorder + RightBlockIterator].dArr[i])
+                    {
+                        SortedBlock[LeftBlockIterator + RightBlockIterator] = arr[LeftBorder + LeftBlockIterator].dArr[i];
+                        LeftBlockIterator++;
+                    }
+                    else
+                    {
+                        SortedBlock[LeftBlockIterator + RightBlockIterator] = arr[MidBorder + RightBlockIterator].dArr[i];
+                        RightBlockIterator++;
+                    }
+                }
+                while (LeftBorder + LeftBlockIterator < MidBorder)
+                {
+                    SortedBlock[LeftBlockIterator + RightBlockIterator] = arr[LeftBorder + LeftBlockIterator].dArr[i];
+                    LeftBlockIterator++;
+                }
+                while (MidBorder + RightBlockIterator < RightBorder)
+                {
+                    SortedBlock[LeftBlockIterator + RightBlockIterator] = arr[MidBorder + RightBlockIterator].dArr[i];
+                    RightBlockIterator++;
+                }
+                
+                for (int MergeIterator = 0; MergeIterator < LeftBlockIterator + RightBlockIterator; MergeIterator++)
+                {
+                    arr[LeftBorder + MergeIterator].dArr[i] = SortedBlock[MergeIterator];
+                }
+                delete[] SortedBlock;
+                
+            }
+            
+        }
+    }
+}
+
+
+void CalcArray(Array arr[])
+{
+    double dArr[n];
+       int i, j;
+       int min;
+       int min_index_1, min_index_2;
+    
+       min = dArr[0][0];
+    
+       for ( i = 0; i < 5; i++)
+           for ( j = 1; j < 5; j++)
+               if(min > dArr[i][j])
+               {
+                   min = dArr[i][j];
+                   min_index_1 = i;
+                   min_index_2 = j;
+               }
+               std::cout <<"Min element " << min << std::endl;
+               std::cout << "Index " << "[" << min_index_1 << "][" << min_index_2 << "]" << std::endl;
+    }
+
+
+int main()
+{
+    cout << "Enter elements of matrix:\n" << endl;
+    Array  arr[n];
+    InputArray(arr);
+    cout << "\nYour matrix:\n" << endl;
+    OutputArray(arr);
+    SortArray(arr);
+    cout << "\nSorted matrix:\n" << endl;
+    OutputArray(arr);
+    cout << "\nCalculations:\n" << endl;
+    CalcArray(arr);
+}
+
